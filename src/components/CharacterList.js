94e772e3d68c37ea7bd4f30f2759ce2c891be552
chudnofskyATD <mpereannor@gmail.com>
2019-09-20 08:39:13 +0000

    character list set up version 1

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    // const dependencyArray = 
+
+    const getCharacters = () => {
+    axios
+    .get(`https://rick-api.herokuapp.com/api/character `)
+    .then(response => {
+      setCharacters(response.data.results)
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  };
+  getCharacters();
+}, []);
+
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>
+        {
+          characters.map(character => (
+            <div> character={character}</div>
+          ))
+        }
+      </h2>
     </section>
   );
-}
+};
+
+export default CharacterList;
