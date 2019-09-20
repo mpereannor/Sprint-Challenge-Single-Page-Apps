5e353ab8ea5aab2f67350b42153da994d847a784
chudnofskyATD <mpereannor@gmail.com>
2019-09-20 13:56:35 +0000

    searched form created

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 import  WelcomePage from "./components/WelcomePage";
-import {Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import SearchForm from "./components/SearchForm.js";
 
 export default function App() {
   return (
-    <main>
-      <Header />
-      <Route exact path='/' component={WelcomePage}/>
-      <Route exact path='/characters' component={CharacterList}/>
-    </main>
+    <Router>
+      <main>
+        <Header />
+        <Route exact path='/' component={WelcomePage}/>
+        <Route exact path='/characters' component={CharacterList}/>
+        <SearchForm/>
+      </main>
+    </Router>
   );
 }
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,41 @@
 import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
-export default function SearchForm() {
+const validationSchema = yup.object().shape({
+  name: yup.string()
+  .required('invalid format')});
+
+
+
+export default function SearchForm(onSubmit) {
  
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+    <Formik 
+        validationSchema={validationSchema}
+        onSubmit = {onSubmit}
+
+        render ={ () => {
+          return (
+            <Form>
+              <section className="search-form">
+                <label>
+                    Search By Name 
+                    <input type="text" placeholder="where's summer?" />
+                    <ErrorMessage name='name' component='div'/>
+                </label>
+                  
+              </section>  
+            </Form>
+
+          )
+
+        } 
+
+    }
+    />
+  )
 }
+      
+     // Add a search form here
+
